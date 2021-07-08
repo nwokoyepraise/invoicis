8a11aeb9d18eb87b4e2b7ea5b632a031e5284c04
nwokoyepraise <nwokoyepraise@gmail.com>
2021-07-08 16:17:12 +0100

    remove redundant imports

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const port_number = Number.parseInt(process.env.PORT_NUMBER);
-const pdf_doc = require('pdfkit');
-const fs = require('fs');
+
+
+
+
+
 
 
 server.listen(port_number, () => {
