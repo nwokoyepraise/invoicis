b9c821a0df08d11e7dba764e369d7ddd116dd4d2
nwokoyepraise <nwokoyepraise@gmail.com>
2021-07-10 09:04:04 +0100

    change border color

--- api/gen_invoice.js
+++ api/gen_invoice.js
@@ -1,7 +1,8 @@
 const template_0001 = require('../templates/template_0001');
+const template_0002 = require('../templates/template_0002')
 
 module.exports = async function (app) {
     //app.post('/api/gen_invoice', function (req, res) {
-    template_0001.gen();
+    template_0002.gen();
     // })
 }
