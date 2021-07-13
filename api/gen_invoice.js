d3dbf917546d2552cdc9d175eff92e6a4dbadaac
nwokoyepraise <nwokoyepraise@gmail.com>
2021-07-14 00:22:48 +0100

    output both template samples

--- api/gen_invoice.js
+++ api/gen_invoice.js
@@ -3,6 +3,7 @@ const template_0002 = require('../templates/template_0002')
 
 module.exports = async function (app) {
     //app.post('/api/gen_invoice', function (req, res) {
+    template_0001.gen();
     template_0002.gen();
     // })
 }
