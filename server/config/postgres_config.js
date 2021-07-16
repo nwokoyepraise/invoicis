1803017cff40380f364b08bc667c57bcdddb380d
nwokoyepraise <nwokoyepraise@gmail.com>
2021-07-16 15:16:34 +0100

    config file for node-postgres

---
+++ server/config/postgres_config.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+const isProoduction = process.env.NODE_ENV === 'production';
+
+const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`
+
+const pool = new Pool({
+    connectionString: isProoduction ? process.env.DATABASE_URL : connectionString,
+    ssl: isProoduction
+});
+
+module.exports = { pool }
