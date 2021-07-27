635d10a2057ca9144ea0c4b6c179b9da1f965723
nwokoyepraise <nwokoyepraise@gmail.com>
2021-07-27 21:48:35 +0100

    gen user_id and/or string with wildcard length

--- server/utils/crypt_gen.js
+++ server/utils/crypt_gen.js
@@ -1,6 +1,16 @@
 const cryptoRandomString = require('crypto-random-string');
 
+module.exports.gen = function (length) {
+    const mChar = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    return cryptoRandomString({ length: length, characters: mChar });
+}
+
 module.exports.gen_invoice_no = function () {
     const mChar = '0123456789';
     return cryptoRandomString({ length: 20, characters: mChar });
+}
+
+module.exports.gen_user_id = function () {
+    const mChar = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    return cryptoRandomString({ length: 12, characters: mChar });
 }
