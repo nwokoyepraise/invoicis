0e8071fa7e5342f826621ba4c2487276462431b7
nwokoyepraise <nwokoyepraise@gmail.com>
2021-08-28 10:28:22 +0100

    use express router

--- server/app.js
+++ server/app.js
@@ -3,26 +3,24 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const port_number = Number.parseInt(process.env.PORT_NUMBER);
+const gen_invoice = require('./routes/invoice_routes/gen_invoice');
+const user_reg = require('./routes/user_routes/user_reg');
+const user_auth = require('./routes/user_routes/user_auth');
+const user_key = require('./routes/user_routes/user_key');
+const user_update = require('./routes/user_routes/user_update');
+const user_profile = require('./routes/user_routes/user_profile');
+
 
 app.use(express.json({ limit: '20kb' }));
 
-const gen_invoice = require('./api/invoice_handler/gen_invoice');
-gen_invoice(app);
+//load routes
+app.use('/user_reg', user_reg);
+app.use('/user_login', user_auth.user_login);
+app.use('/profile_update', user_update.profile_update);
+app.use('/user_data/get_details/profile', user_profile.get_user_profile);
+app.use('/api/gen_invoice', gen_invoice);
+app.use('/api/gen_key', user_key.gen_api_key);
 
-const user_reg = require('./api/user_handler/user_reg');
-user_reg(app);
-
-const user_auth = require('./api/user_handler/user_auth');
-user_auth.user_login(app);
-
-const user_key = require('./api/user_handler/user_key');
-user_key.gen_api_key(app)
-
-const user_update = require('./api/user_handler/user_update');
-user_update(app);
-
-const user_profile = require('./api/user_handler/user_profile');
-user_profile.get_user_profile(app);
 
 server.listen(port_number, () => {
     console.log(`server listening on port ${port_number}`);
